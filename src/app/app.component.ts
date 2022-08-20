dfda45b70e0a89bc22924a659f0f2f945392a456
sontonyD <--get>
2022-08-20 08:15:44 +0200

    tablet design: crewPage OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('screen') containerElement!: ElementRef;
   
   isMenuOpen = false;
-  currentPage = "destination";
+  currentPage = "crew";
 
   constructor(private renderer : Renderer2) { }
 
